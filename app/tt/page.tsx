b7a6277a9a1d36417b1bbe35c7354d9a1416eea7
hjr <757671515@qq.com>
2024-12-20 14:12:00 +0800

    tt

---
+++ app/tt/page.tsx
@@ -0,0 +1,14 @@
+
+export default async function TT() {
+    const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+    const genAI = new GoogleGenerativeAI("AIzaSyDUu9RLhvDgCPCA1woklhU4zcDDPP6QmWc");
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    
+    const prompt = "Explain how AI works";
+    
+    const result = await model.generateContent(prompt);
+    console.log(result.response.text());
+    return <div>{result.response.text()}</div>
+}
+
