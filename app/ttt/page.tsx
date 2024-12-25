4d168abd6102ea3cb747e1fd381974429308a596
hjr <757671515@qq.com>
2024-12-25 21:55:43 +0800

    1

--- app/ttt/page.tsx
+++
@@ -1,7 +0,0 @@
-export default async function TTT() {
-    const res=await fetch("http://localhost:3000/api/hello")
-    const data=await res.json()
-    console.log(data)
-  
-    return <div>{data.message}</div>
-}
